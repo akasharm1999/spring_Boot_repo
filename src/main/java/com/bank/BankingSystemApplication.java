90e6ce528f9a4dc01580d2ef4b2bf15ab63ae175
Akash Sharma <akasharma912@gmail.com>
2025-03-23 22:45:44 +0530

    misc3

--- src/main/java/com/bank/BankingSystemApplication.java
+++ src/main/java/com/bank/BankingSystemApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankingSystemApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(BankingSystemApplication.class, args);
 	}
