d25fbd4c5d3ace1ece798ab4afa9c2865e1b42c8
Akash Sharma <akasharm1999@gmail.com>
2025-03-23 21:53:15 +0530

    misc

--- src/main/java/com/bank/controller/UserController.java
+++ src/main/java/com/bank/controller/UserController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;  
+
+
 
 import com.bank.model.UserEntity;
 import com.bank.service.UserService;
